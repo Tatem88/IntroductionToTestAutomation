48376a3ed1c7031384dddd5ba7d2697420fb41bf
geekbrains <kuznecova_tat88@mail.ru>
2023-12-17 21:10:59 +0300

    first commit

--- src/main/java/Homework/Lesson1/MontyHallParadoxDemo.java
+++ src/main/java/Homework/Lesson1/MontyHallParadoxDemo.java
@@ -1,2 +1,49 @@
-package Homework.Lesson1;public class MontyHallParadoxDemo {
+package Homework.Lesson1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MontyHallParadoxDemo {
+
+    public static void main(String[] args) {
+
+        int totalGames = 1000;
+        int switchWins = 0;
+        int stayWins = 0;
+
+        for (int i = 0; i < totalGames; i++) {
+            List<Integer> doors = new ArrayList<>();
+            doors.add(0);
+            doors.add(0);
+            doors.add(1);
+            Collections.shuffle(doors);
+
+            int chosenDoor = (int) (Math.random() * 3);
+            int revealedDoor;
+            do {
+                revealedDoor = (int) (Math.random() * 3);
+            } while (revealedDoor == chosenDoor || doors.get(revealedDoor) == 1);
+
+            // игрок меняет выбор двери
+            int newChosenDoor;
+            do {
+                newChosenDoor = (int) (Math.random() * 3);
+            } while (newChosenDoor == chosenDoor || newChosenDoor == revealedDoor);
+
+            if (doors.get(newChosenDoor) == 1) {
+                switchWins++;
+            }
+
+            // игрок не меняет выбор двери
+            if (doors.get(chosenDoor) == 1) {
+                stayWins++;
+            }
+        }
+
+        System.out.println("При смене выбора двери: " + switchWins + " выигрышей из " + totalGames + " игр");
+        System.out.println("При несменяемости выбора двери: " + stayWins + " выигрышей из " + totalGames + " игр");
+    }
 }
+
+
--- src/test/java/Homework/Lesson1/MontyHallParadoxDemoTest.java
+++ src/test/java/Homework/Lesson1/MontyHallParadoxDemoTest.java
@@ -1,2 +1,49 @@
-package Homework.Lesson1;public class MontyHallParadoxDemoTest {
+package Homework.Lesson1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class MontyHallParadoxDemoTest {
+    private final int totalGames = 1000; // общее количество игр
+    private final int switchWins = 600; // количество выигрышей при смене выбора двери
+    private final int stayWins = 400; // количество выигрышей при несменяемости выбора двери
+
+    @Test
+    public void positiveTests() {
+        Assert.assertTrue(switchWins > 0);
+        Assert.assertTrue(stayWins > 0);
+        Assert.assertEquals(totalGames, switchWins + stayWins);
+    }
+    @Test
+    public void testStayWinsGreaterThanZero() {
+        int stayWins = 0;
+        // Perform test logic here and update stayWins variable
+
+        assertTrue(stayWins > 0);
+    }
+
+    @Test
+    public void testTotalGamesEqualsSwitchWinsPlusStayWins() {
+        assertEquals(totalGames, switchWins + stayWins);
+    }
+
+    @Test
+    public void negativeTests() {
+        Assert.assertTrue(totalGames > 0);
+        Assert.assertEquals(totalGames, switchWins + stayWins);
+        Assert.assertTrue(switchWins >= 0 && stayWins >= 0);
+    }
+    @Test
+    public void testTotalGamesGreaterThanZero() {
+        int totalGames = 5;
+        assert totalGames > 0;
+    }
+    @Test
+    public void testWins() {
+        assertTrue(switchWins >= 0 && stayWins >= 0);
+    }
 }
+
